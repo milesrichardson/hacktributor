67151286c6d0709825e95714c1e3cbe9164f5281
Miles Richardson <milesrichardson@gmail.com>
2018-02-23 17:55:12 +0000

    use proper email/username when running in docker

--- feel_better_about_myself.sh
+++ feel_better_about_myself.sh
@@ -3,8 +3,8 @@
 THIS_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 TMP_GIT_CONFIG_FILE="$THIS_DIR/.tmp_git_config"
 
-GIT_NAME="$(git config --global --list | grep 'user.name')"
-GIT_EMAIL="$(git config --global --list | grep 'user.email')"
+GIT_NAME="$(git config --global --list | grep 'user.name' | cut -d'=' -f2-)"
+GIT_EMAIL="$(git config --global --list | grep 'user.email' | cut -d'=' -f2-)"
 
 echo "[user]" > "$TMP_GIT_CONFIG_FILE"
 echo "    name = $GIT_NAME" >> "$TMP_GIT_CONFIG_FILE"
